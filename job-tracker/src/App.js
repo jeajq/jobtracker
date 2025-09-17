2c63d6bae2f9d69ea891d3045dfbfc7a233333cc
Jean <98675442+jeajq@users.noreply.github.com>
2025-09-17 11:51:08 +1000

    Cleaned up comments in files

--- job-tracker/src/App.js
+++ job-tracker/src/App.js
@@ -4,11 +4,3 @@ export default function App() {
   return <JobTracker />;
 }
 
-/*import React from "react";
-import JobTracker from "./components/JobTracker";
-import SeedJobs from "./dev/SeedJobs";
-
-export default function App() {
-  const showSeed = window.location.search.includes("seed");
-  return showSeed ? <SeedJobs /> : <JobTracker />;
-}*/
